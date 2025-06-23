118aa8290da56375afb99fc4fa51b596751fa009
Damares Gaia <damaresgaia@gmail.com>
2025-06-23 08:56:59 -0300

    chore: remove comentários desnecessários do código

--- Molieres.java
+++ Molieres.java
@@ -13,51 +13,46 @@ public class Molieres extends AdvancedRobot {
 	private double energiaAnterior = 100;
     private long ultimoTickDefesa = 0;
 
-	int count = 0; // Keeps track of how long we've
-	// been searching for our target
-	double gunTurnAmt; // How much to turn our gun when searching
-	String trackName; // Name of the robot we're currently tracking
+	int count = 0; 
+	double gunTurnAmt;
+	String trackName; 
 
-	/**
-	 * run:  Tracker's main run function
-	 */
+	
 	public void run() {
-		// Set colors
+		
 		setBodyColor(new Color(128, 128, 50));
 		setGunColor(new Color(50, 50, 20));
 		setRadarColor(new Color(200, 200, 70));
 		setScanColor(Color.white);
 		setBulletColor(Color.blue);
 
-		// Prepare gun
-		trackName = null; // Initialize to not tracking anyone
-		setAdjustGunForRobotTurn(true); // Keep the gun still when we turn
-		gunTurnAmt = 10; // Initialize gunTurn to 10
+		
+		trackName = null; 
+		setAdjustGunForRobotTurn(true); 
+		gunTurnAmt = 10; 
 
-		// Loop forever
+		
 		while (true) {
-			// turn the Gun (looks for enemy)
+			
 			turnGunRight(gunTurnAmt);
-			// Keep track of how long we've been looking
+			
 			count++;
-			// If we've haven't seen our target for 2 turns, look left
+			
 			if (count > 2) {
 				gunTurnAmt = -10;
 			}
-			// If we still haven't seen our target for 5 turns, look right
+			
 			if (count > 5) {
 				gunTurnAmt = 10;
 			}
-			// If we *still* haven't seen our target after 10 turns, find another target
+			
 			if (count > 11) {
 				trackName = null;
 			}
 		}
 	}
 
-	/**
-	 * onScannedRobot:  Here's the good stuff
-	 */
+	
 	public void onScannedRobot(ScannedRobotEvent e) {
 
 
@@ -72,30 +67,30 @@ public class Molieres extends AdvancedRobot {
 			return;
 		}
 
-		// If we don't have a target, well, now we do!
+		
 		if (trackName == null) {
 			trackName = e.getName();
 			out.println("Tracking " + trackName);
 		}
-		// This is our target.  Reset count (see the run method)
+		
 		count = 0;
-		// If our target is too far away, turn and move toward it.
+	
 		if (e.getDistance() > 150) {
 			gunTurnAmt = normalRelativeAngleDegrees(e.getBearing() + (getHeading() - getRadarHeading()));
 
-			turnGunRight(gunTurnAmt); // Try changing these to setTurnGunRight,
-			turnRight(e.getBearing()); // and see how much Tracker improves...
-			// (you'll have to make Tracker an AdvancedRobot)
+			turnGunRight(gunTurnAmt); 
+			turnRight(e.getBearing()); 
+			
 			ahead(e.getDistance() - 140);
 			return;
 		}
 
-		// Our target is close.
+		
 		gunTurnAmt = normalRelativeAngleDegrees(e.getBearing() + (getHeading() - getRadarHeading()));
 		turnGunRight(gunTurnAmt);
 		fire(3);
 
-		// Our target is too close!  Back up.
+		
 		if (e.getDistance() < 100) {
 			if (e.getBearing() > -90 && e.getBearing() <= 90) {
 				back(40);
@@ -111,15 +106,13 @@ public class Molieres extends AdvancedRobot {
     }
 
 	public void onHitRobot(HitRobotEvent e) {
-		// Only print if he's not already our target.
+		
 		if (trackName != null && !trackName.equals(e.getName())) {
 			out.println("Tracking " + e.getName() + " due to collision");
 		}
-		// Set the target
+		
 		trackName = e.getName();
-		// Back up a bit.
-		// Note:  We won't get scan events while we're doing this!
-		// An AdvancedRobot might use setBack(); execute();
+		
 		gunTurnAmt = normalRelativeAngleDegrees(e.getBearing() + (getHeading() - getRadarHeading()));
 		turnGunRight(gunTurnAmt);
 		fire(3);
@@ -136,10 +129,6 @@ public class Molieres extends AdvancedRobot {
     setAhead(120 + Math.random() * 80); // Avança entre 120 e 200
 }
 
-
-	/**
-	 * onWin:  Do a victory dance
-	 */
 	public void onWin(WinEvent e) {
 		for (int i = 0; i < 50; i++) {
 			turnRight(30);
