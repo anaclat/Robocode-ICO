1834ca995a58a266aaa60533523ea00aec4ff45f
noemiDsouza <161595097+noemiDsouza@users.noreply.github.com>
2025-06-30 18:16:34 +0000

    cria classe Robo que salva posição, energia, direção e dados dos robôs para usar nas estratégias de batalha

--- Molieres.java
+++ Molieres.java
@@ -9,5 +9,20 @@ import robocode.*;
 import robocode.util.Utils;
 
 public class Molieres extends AdvancedRobot {
+    static double POTENCIA_TIRO = 3;
+
+    class Robo extends Point2D.Double {
+        public long tempoVarredura; 
+        public boolean vivo = true;
+        public double energia;
+        public String nome;
+        public double anguloCanhaoRadianos;
+        public double anguloAbsolutoRadianos;
+        public double velocidade;
+        public double direcao;
+        public double ultimaDirecao;
+        public double pontuacaoDisparo;
+        public double distancia; 
+    }
     
 }
